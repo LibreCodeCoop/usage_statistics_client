1de86d4063d8fd3587a9ccbf74237f372220b725
Vitor Mattos <vitor@php.rio>
2026-09-08 18:03:27 -0300

    test: lock installation id derivation vector
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/InstallationIdTest.php
+++ tests/InstallationIdTest.php
@@ -20,6 +20,7 @@ final class InstallationIdTest extends TestCase
         $second = (string)InstallationId::derive('libresign', 'local-instance-id');
         $otherApp = (string)InstallationId::derive('talk', 'local-instance-id');
 
+        self::assertSame('dc8adebdce9ab99790a7d037f44965b4ca7b6dceb38d5e7c9dff118721fc8415', $first);
         self::assertSame($first, $second);
         self::assertNotSame($first, $otherApp);
         self::assertMatchesRegularExpression('/^[a-f0-9]{64}$/', $first);
